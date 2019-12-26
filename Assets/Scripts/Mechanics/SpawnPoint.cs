75abd8a8bd8a4365360679ab3de565f75e9518fc
nobrayner <braydon@raynerhall.co>
2019-12-26 16:07:04 +1100

    Added Scripts for Reference
    Copied in scripts from the Micro-Game Tutorial project for a 2D Platformer game.

---
+++ Assets/Scripts/Mechanics/SpawnPoint.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace Platformer.Mechanics {
+    /// <summary>
+    /// Marks a gameobject as a spawnpoint in a scene.
+    /// </summary>
+    public class SpawnPoint : MonoBehaviour {
+
+    }
+}
